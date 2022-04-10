0d752eafc40b39bbff69846c184e6bcf77c0df4c
Lander Brandt <landerbrandt@gmail.com>
2022-04-09 19:13:27 -0700

    code restructuring

--- src/driver.rs
+++ src/driver.rs
@@ -1,13 +1,14 @@
-use rand::prelude::StdRng;
+use rand::{prelude::{StdRng, IteratorRandom}, Rng};
+use sha1::{Digest, Sha1};
 
-use crate::{exports::fazi_initialize, sancov::{CoverageMap, PcEntry}, Fazi, options::RuntimeOptions};
+use crate::{exports::fazi_initialize, sancov::{CoverageMap, PcEntry}, Fazi, options::RuntimeOptions, weak_imports::*};
 use std::{
     collections::HashSet,
     lazy::SyncOnceCell,
     sync::{
-        atomic::{AtomicBool, AtomicU8, AtomicUsize},
+        atomic::{AtomicBool, AtomicU8, AtomicUsize, Ordering},
         Arc, Mutex,
-    },
+    }, path::Path,
 };
 
 pub(crate) static CONSTANTS: SyncOnceCell<Mutex<CoverageMap>> = SyncOnceCell::new();
@@ -21,11 +22,10 @@ pub(crate) static mut PC_INFO: Option<&'static [PcEntry]> = None;
 #[cfg(feature = "main_entrypoint")]
 #[no_mangle]
 extern "C" fn main() {
-    use std::sync::atomic::Ordering;
 
     use clap::StructOpt;
 
-    use crate::{weak_imports::libfuzzer_runone_fn, exports::update_coverage};
+    use crate::{weak_imports::libfuzzer_runone_fn};
 
     fazi_initialize();
 
@@ -54,5 +54,207 @@ extern "C" fn main() {
         let res = unsafe { f(fazi.input.as_ptr(), fazi.input.len()) };
 
         fazi.end_iteration(res != 0);
+    }
+}
+
+impl<R: Rng> Fazi<R> {
+    pub(crate) fn start_iteration(&mut self) {
+        self.poison_input();
+        self.update_max_size();
+    }
+
+    pub(crate) fn update_max_size(&mut self) {
+        // Update the max length
+        if self.options.len_control > 0 && self.current_max_mutation_len < self.max_input_size {
+            let max_mutation_len_f64: f64 = self.max_input_size as f64;
+            let len_control: f64 = self
+                .options
+                .len_control
+                .try_into()
+                .expect("failed to convert len_control");
+            let factor = (len_control * max_mutation_len_f64.log10()).trunc() as usize;
+            if self.iterations - self.last_corpus_update_run > factor {
+                let max_mutation_len = self.max_input_size;
+                let new_max_mutation_len =
+                    max_mutation_len + (max_mutation_len_f64.log10() as usize);
+                self.max_input_size = new_max_mutation_len;
+            }
+        }
+
+        self.current_max_mutation_len = std::cmp::max(self.input.len(), self.max_input_size);
+    }
+
+    pub(crate) fn end_iteration(&mut self, need_more_data: bool) {
+        self.unpoison_input();
+
+        let (new_coverage, old_coverage) = update_coverage();
+
+        if !need_more_data {
+            let min_input_size = if let Some(min_input_size) = self.min_input_size {
+                std::cmp::min(self.input.len(), min_input_size)
+            } else {
+                self.input.len()
+            };
+
+            self.min_input_size = Some(min_input_size);
+        }
+
+        let can_request_more_data = !self.min_input_size.is_some();
+
+        if old_coverage != new_coverage {
+            println!(
+                "old coverage: {}, new coverage: {}, mutations: {:?}",
+                old_coverage, new_coverage, self.mutations
+            );
+
+            self.corpus.push(self.input.clone());
+
+            let input = self.input.clone();
+            let corpus_dir = self.options.corpus_dir.clone();
+            std::thread::spawn(move || {
+                save_input(corpus_dir.as_ref(), input.as_slice());
+            });
+
+            self.current_mutation_depth = 0;
+            self.mutations.clear();
+
+            let mut constants = CONSTANTS
+                .get()
+                .expect("failed to get CONSTANTS")
+                .lock()
+                .expect("failed to lock CONSTANTS");
+            constants.clear();
+        } else if self.current_mutation_depth == self.options.max_mutation_depth
+            && (!need_more_data || !can_request_more_data)
+        {
+            if let Some(input) = self
+                .corpus
+                .iter()
+                .filter(|input| input.len() >= self.min_input_size.unwrap_or(0))
+                .choose(&mut self.rng)
+            {
+                self.input = input.clone();
+                self.current_mutation_depth = 0;
+                self.mutations.clear();
+
+                let mut constants = CONSTANTS
+                    .get()
+                    .expect("failed to get CONSTANTS")
+                    .lock()
+                    .expect("failed to lock CONSTANTS");
+                constants.clear();
+            }
+        }
+
+        let mutation = if need_more_data && can_request_more_data {
+            self.extend_input()
+        } else {
+            self.mutate_input()
+        };
+        self.mutations.push(mutation);
+        self.current_mutation_depth += 1;
+
+        let mut last_input = LAST_INPUT
+            .get()
+            .expect("LAST_INPUT not initialized")
+            .lock()
+            .expect("failed to lock LAST_INPUT");
+        *last_input = Arc::clone(&self.input);
+        drop(last_input);
+
+        self.iterations += 1;
+
+        if self.iterations % 1000 == 0 {
+            println!("iter: {}", self.iterations);
+        }
+    }
+
+    /// Marks all bytes of the input buffer's allocated data as addressable
+    pub(crate) fn unpoison_input(&mut self) {
+        let input_ptr = self.input.as_ptr();
+
+        if let Some(asan_unpoison) = asan_unpoison_memory_region_fn() {
+            unsafe {
+                asan_unpoison(input_ptr, self.input.capacity());
+            }
+        }
+
+        if let Some(msan_unpoison) = msan_unpoison_memory_region_fn() {
+            unsafe {
+                msan_unpoison(input_ptr, self.input.capacity());
+            }
+        }
+    }
+
+    /// Marks the difference between the input's buffer's length and capacity as
+    /// unaddressable
+    pub(crate) fn poison_input(&mut self) {
+        let unaddressable_bytes = self.input.capacity() - self.input.len();
+        let unaddressable_start = unsafe { self.input.as_ptr().offset(self.input.len() as isize) };
+
+        if let Some(asan_poison) = asan_poison_memory_region_fn() {
+            unsafe {
+                asan_poison(unaddressable_start, unaddressable_bytes);
+            }
+        }
+
+        if let Some(msan_unpoison) = msan_poison_memory_region_fn() {
+            unsafe {
+                msan_unpoison(unaddressable_start, unaddressable_bytes);
+            }
+        }
+    }
+}
+
+pub(crate) fn update_coverage() -> (usize, usize) {
+    let mut coverage = COVERAGE
+        .get()
+        .expect("failed to get COVERAGE")
+        .lock()
+        .expect("failed to lock COVERAGE");
+
+    let old_coverage = coverage.len();
+    for (idx, counter) in unsafe { U8_COUNTERS.as_ref().unwrap().iter().enumerate() } {
+        if counter.load(Ordering::Relaxed) > 0 {
+            // Grab the PC corresponding tot his entry
+            let pc_info = unsafe { &PC_INFO.as_ref().unwrap()[idx] };
+
+            coverage.insert(pc_info.pc);
+        }
+    }
+
+    (coverage.len(), old_coverage)
+}
+
+pub(crate) fn handle_crash(crashes_dir: &Path, input: &[u8]) {
+    let mut hasher = Sha1::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+    let filename = hex::encode(result.as_slice());
+    let crash_file_path = crashes_dir.join(&filename);
+
+    eprintln!("Received SIGABRT -- saving crash to {:?}", crash_file_path);
+    ensure_parent_dir_exists(crash_file_path.as_ref());
+
+    std::fs::write(crash_file_path, input).expect("failed to save crash file!");
+}
+
+pub(crate) fn save_input(corpus_dir: &Path, input: &[u8]) {
+    let mut hasher = Sha1::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+    let filename = hex::encode(result.as_slice());
+    let corpus_file_path = corpus_dir.join(&filename);
+
+    eprintln!("Saving corpus input to {:?}", corpus_file_path);
+    ensure_parent_dir_exists(corpus_file_path.as_ref());
+
+    std::fs::write(corpus_file_path, input).expect("failed to save corpus input file!");
+}
+
+fn ensure_parent_dir_exists(path: &Path) {
+    let parent = path.parent().expect("path has no parent directory?");
+    if !parent.exists() {
+        std::fs::create_dir_all(parent).expect("failed to create_dir_all on parent directory");
     }
 }
--- src/signal.rs
+++ src/signal.rs
@@ -5,7 +5,7 @@ use rand::Rng;
 use sha1::{Digest, Sha1};
 use signal_hook::iterator::Signals;
 
-use crate::{driver::LAST_INPUT, Fazi};
+use crate::{driver::{LAST_INPUT, handle_crash, save_input}, Fazi};
 
 impl<R: Rng> Fazi<R> {
     pub(crate) fn setup_signal_handler(&self) {
@@ -26,38 +26,5 @@ impl<R: Rng> Fazi<R> {
                 }
             }
         });
-    }
-}
-
-pub(crate) fn handle_crash(crashes_dir: &Path, input: &[u8]) {
-    let mut hasher = Sha1::new();
-    hasher.update(input);
-    let result = hasher.finalize();
-    let filename = hex::encode(result.as_slice());
-    let crash_file_path = crashes_dir.join(&filename);
-
-    eprintln!("Received SIGABRT -- saving crash to {:?}", crash_file_path);
-    ensure_parent_dir_exists(crash_file_path.as_ref());
-
-    std::fs::write(crash_file_path, input).expect("failed to save crash file!");
-}
-
-pub(crate) fn save_input(corpus_dir: &Path, input: &[u8]) {
-    let mut hasher = Sha1::new();
-    hasher.update(input);
-    let result = hasher.finalize();
-    let filename = hex::encode(result.as_slice());
-    let corpus_file_path = corpus_dir.join(&filename);
-
-    eprintln!("Saving corpus input to {:?}", corpus_file_path);
-    ensure_parent_dir_exists(corpus_file_path.as_ref());
-
-    std::fs::write(corpus_file_path, input).expect("failed to save corpus input file!");
-}
-
-fn ensure_parent_dir_exists(path: &Path) {
-    let parent = path.parent().expect("path has no parent directory?");
-    if !parent.exists() {
-        std::fs::create_dir_all(parent).expect("failed to create_dir_all on parent directory");
     }
 }
