a9b95a13c1ff34d15411cfa41c53a353f85ee295
Lander Brandt <landerbrandt@gmail.com>
2022-04-12 00:45:17 -0700

    Change sample harness to use env var for handling panics

--- examples/simple_harness.rs
+++ examples/simple_harness.rs
@@ -1,16 +1,28 @@
+use std::env;
+
 use fazi::{Fazi, FaziBuilder};
 use rand::prelude::StdRng;
 
 fn main() {
-    FaziBuilder::<StdRng>::default()
-        .handle_panics() // Ensure that we treat panics as crashes
+    let fazi = FaziBuilder::<StdRng>::default()
         .handle_signals() // Ensure that we catch certain system signals that should be treated as crashes
         .do_recoverage() // Rerun existing inputs before fuzzing
         .do_fuzzing() // Run the fuzzer after doing recoverage
         .harness(&buggy_function) // Our target function
-        .run();
+        ;
+        let handle_panics = if let Some(env_var_value) = env::var_os("FAZI_PANIC_HANDLER") {
+            env_var_value != "off"
+        } else {
+            true
+        };
+        if handle_panics {
+            fazi.handle_panics()
+        } else {
+            fazi
+        }.run();
 }
 
+#[inline(never)]
 fn buggy_function(input: &[u8]) {
     if input.len() >= 5
         && input[0] == b'h'
